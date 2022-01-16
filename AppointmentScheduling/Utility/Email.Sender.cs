17ac2f57df54b15232a17b57a738e3062e652e2d
Deniz ÖZÇELİK <45967316+denizozcelik@users.noreply.github.com>
2022-01-16 22:33:10 +0300

    MailJet Entegration

---
+++ AppointmentScheduling/Utility/Email.Sender.cs
@@ -0,0 +1,40 @@
+﻿using Mailjet.Client;
+using Mailjet.Client.Resources;
+using Microsoft.AspNetCore.Identity.UI.Services;
+using Newtonsoft.Json.Linq;
+using System.Threading.Tasks;
+
+namespace AppointmentScheduling.Utility
+{
+    public class EmailSender : IEmailSender
+    {
+        public async Task SendEmailAsync(string email, string subject, string htmlMessage)
+        {
+            MailjetClient client = new MailjetClient("YourApiKey", "YourApiSecret")
+            {
+
+            };
+            MailjetRequest request = new MailjetRequest
+            {
+                Resource = Send.Resource,
+            }
+               .Property(Send.Messages, new JArray {
+                new JObject {
+                 {"From", new JObject {
+                  {"Email", "senderMailAdress@gmail.com"},
+                  {"Name", "Appointment Scheduler"}
+                  }},
+                 {"To", new JArray {
+                  new JObject {
+                   {"Email", email},
+                   }
+                  }},
+                 {"Subject", subject},
+
+                 {"HTMLPart", htmlMessage}
+                 }
+                   });
+            MailjetResponse response = await client.PostAsync(request);
+        }
+    }
+}
